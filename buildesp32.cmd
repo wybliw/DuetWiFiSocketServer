44cfdcd161db865a33c7335e225b8439e3bf3898
Andy Shaw <andy-git@gloomy-place.com>
2021-04-15 23:00:36 +0100

    Add build file

---
+++ buildesp32.cmd
@@ -0,0 +1,9 @@
+
+
+rem first build the project using cmake/idf.py
+idf.py clean
+idf.py all
+rem now generate the combined binary image
+cd build
+esptool.py --chip esp32 merge_bin -o DuetWiFiServer.bin --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 bootloader/bootloader.bin 0x10000 DuetWiFiSocketServer.bin 0x8000 partition_table/partition-table.bin
+cd ..
