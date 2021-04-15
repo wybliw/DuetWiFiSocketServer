053b545f9a253eb8ca4253cb46bf726ff3453572
Andy Shaw <andy-git@gloomy-place.com>
2021-04-15 22:59:01 +0100

    Remove old files

--- makefileesp32.mk
+++
@@ -1,8 +0,0 @@
-OS ?=Windows_NT
-ESP_ROOT?=../esp32
-CHIP?=esp32
-BUILD_DIR?=build
-SKETCH?=src/SocketServer.cpp
-BUILD_EXTRA_FLAGS?=-DDEBUG
-BOARD?=esp32
-include ../makeEspArduino/makeEspArduino.mk
